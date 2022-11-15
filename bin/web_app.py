8909b3d0c6753afa04b9bd83105f883301194e9f
Vladimir Kharchenko <vombat007@gmail.com>
2022-11-15 17:26:58 +0200

    add template and data folder, add web_report code

---
+++ bin/web_app.py
@@ -0,0 +1,71 @@
+import report_racing as rr
+from flask import Flask, request
+from flask import render_template
+from flask import url_for
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['STATIC_FOLDER'] = 'data'
+
+
+def sort_asc_desc(folder, key):
+    """
+    import sort function from report_racing packege and return sort dict by key asc or desc
+    """
+    return rr.sort_report(rr.error_code_and_zero(rr.build_report(folder)), key)
+
+
+@app.route("/")
+def home():
+    """
+    create home page and return html template with title and navigation link
+    """
+    return render_template("base.html", title="WEB Report of Monaco 2018 Racing")
+
+
+@app.route("/report")
+def report():
+    """
+    create report page and return html template with return table with shows common statistic
+    """
+    context = {
+        "title": "common statistic",
+        "test_name": "Report",
+        "report": sort_asc_desc(app.config.get('STATIC_FOLDER'), 'asc')
+    }
+    return render_template("report.html", **context)
+
+
+@app.route("/report/drivers")
+def drivers():
+    """
+    create page and return html template with return  code and name table
+    """
+    context = {
+        "title": "Driver",
+        "test_name": "Report",
+        "report": sort_asc_desc(app.config.get('STATIC_FOLDER'), 'asc')
+    }
+    return render_template("drivers.html", **context)
+
+
+@app.route("/report/drivers/")
+def driver_links():
+    """
+    create page and return html template with return info driver or desc report
+    """
+    driver_id = request.args.get('driver_id')
+    order = request.args.get('order')
+    context = {
+        "report": sort_asc_desc(app.config.get('STATIC_FOLDER'), 'desc'),
+        "driver": driver_id
+    }
+    if order == 'desc':
+        return render_template('report.html', **context)
+    else:
+
+        return render_template('driver_id.html', **context, title='Info About Driver')
+
+
+if __name__ == "__main__":
+    app.run()
