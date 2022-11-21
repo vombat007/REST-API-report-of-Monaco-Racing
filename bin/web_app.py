0d75f48d8abe7a5a24e1166ff0d33a6ab5784c70
Vladimir Kharchenko <vombat007@gmail.com>
2022-11-21 23:16:28 +0200

    add yaml file

--- bin/web_app.py
+++ bin/web_app.py
@@ -2,12 +2,21 @@ import report_racing as rr
 from flask import Flask, request, jsonify
 from flask import render_template, make_response
 from flask_restful import Resource, Api
-from flasgger import Swagger
+from flasgger import Swagger, swag_from
 from flask import url_for
+import json
+import config
 
 app = Flask(__name__)
-api = Api(app)
+
+app.config['SWAGGER'] = {
+    'title': 'API Report Racing',
+    'uiversion': 3,
+    "specs_route": "/swagger/"
+}
 swagger = Swagger(app)
+app.config.from_object(config.Config)
+api = Api(app)
 app.config['DEBUG'] = True
 app.config['STATIC_FOLDER'] = 'data'
 
@@ -19,17 +28,40 @@ def sort_asc_desc(folder, key):
     return rr.sort_report(rr.error_code_and_zero(rr.build_report(folder)), key)
 
 
+
 class Report(Resource):
+    @swag_from('report.yaml', endpoint='report')
     def get(self):
-
+        # """
+        # This examples uses FlaskRESTful Resource
+        # It works also with swag_from, schemas and spec_dict
+        # ---
+        # parameters:
+        #   - in: path
+        #     name: report
+        #     format:
+        #     type: string
+        #     required: true
+        # responses:
+        #   200:
+        #     description: A single user item
+        #     schema:
+        #       id: User
+        #       properties:
+        #         username:
+        #           type: string
+        #           description: The name of the user
+        #           default: Steven Wilson
+        # """
         report = sort_asc_desc(app.config.get('STATIC_FOLDER'), 'asc')
+        j = json.dumps(report)
+        g = json.loads(j)
 
-        return jsonify(report)
+        return
 
 
-
-api.add_resource(Report, '/api/v1/report')
-api.init_app(app)
+api.add_resource(Report, '/report', endpoint='report')
+# api.init_app(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
